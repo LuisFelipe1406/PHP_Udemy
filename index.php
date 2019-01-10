279fe571965c953e0913719efc92fd375502e440
LuisFelipe1406 <44239991+LuisFelipe1406@users.noreply.github.com>
2019-01-10 10:20:21 -0200

    Fixed_Conflict

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
 echo 'Estou usando GIT';
-<<<<<<< HEAD
+
 echo 'Essa alteração está só no VSC';
-=======
 
 echo 'Essa alteração está só no GITHUB';
->>>>>>> 0cb6e85cdb041cd6552f578128620c0062bb2253
 
 ?>
